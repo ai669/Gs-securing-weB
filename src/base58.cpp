822d5ea314c99dba95979e9dca928d2b42a440fe
ai669 <122756256+ai669@users.noreply.github.com>
2020-05-14 15:26:41 +0000

    fixing tests

--- src/base58.cpp
+++ src/base58.cpp
@@ -167,3 +167,214 @@ bool DecodeBase58Check(const char* psz, std::vector<unsigned char>& vchRet) {
         vchRet.clear();
         return false;
     }
+    vchRet.resize(vchRet.size()-4);
+    return true;
+}
+
+bool DecodeBase58Check(const std::string& str, std::vector<unsigned char>& vchRet) {
+    return DecodeBase58Check(str.c_str(), vchRet);
+}
+
+CBase58Data::CBase58Data() {
+    vchVersion.clear();
+    vchData.clear();
+}
+
+void CBase58Data::SetData(const std::vector<unsigned char> &vchVersionIn, const void* pdata, size_t nSize) {
+    vchVersion = vchVersionIn;
+    vchData.resize(nSize);
+    if (!vchData.empty())
+        memcpy(&vchData[0], pdata, nSize);
+}
+
+void CBase58Data::SetData(const std::vector<unsigned char> &vchVersionIn, const unsigned char *pbegin, const unsigned char *pend) {
+    SetData(vchVersionIn, (void*)pbegin, pend - pbegin);
+}
+
+bool CBase58Data::SetString(const char* psz, unsigned int nVersionBytes) {
+    std::vector<unsigned char> vchTemp;
+    bool rc58 = DecodeBase58Check(psz, vchTemp);
+    if ((!rc58) || (vchTemp.size() < nVersionBytes)) {
+        vchData.clear();
+        vchVersion.clear();
+        return false;
+    }
+    vchVersion.assign(vchTemp.begin(), vchTemp.begin() + nVersionBytes);
+    vchData.resize(vchTemp.size() - nVersionBytes);
+    if (!vchData.empty())
+        memcpy(&vchData[0], &vchTemp[nVersionBytes], vchData.size());
+    memory_cleanse(&vchTemp[0], vchData.size());
+    return true;
+}
+
+bool CBase58Data::SetString(const std::string& str) {
+    return SetString(str.c_str());
+}
+
+std::string CBase58Data::ToString() const {
+    std::vector<unsigned char> vch = vchVersion;
+    vch.insert(vch.end(), vchData.begin(), vchData.end());
+    return EncodeBase58Check(vch);
+}
+
+int CBase58Data::CompareTo(const CBase58Data& b58) const {
+    if (vchVersion < b58.vchVersion) return -1;
+    if (vchVersion > b58.vchVersion) return  1;
+    if (vchData < b58.vchData)   return -1;
+    if (vchData > b58.vchData)   return  1;
+    return 0;
+}
+
+namespace {
+    class CWayaWolfCoinAddressVisitor : public boost::static_visitor<bool> {
+    private:
+        CWayaWolfCoinAddress *addr;
+    public:
+        CWayaWolfCoinAddressVisitor(CWayaWolfCoinAddress *addrIn) : addr(addrIn) { }
+
+        bool operator()(const CKeyID &id) const { return addr->Set(id); }
+        bool operator()(const CScriptID &id) const { return addr->Set(id); }
+        bool operator()(const CNoDestination &no) const { return false; }
+        bool operator()(const CStealthAddress &stxAddr) const { return false; }
+    };
+    class CBitcoinAddressVisitor : public boost::static_visitor<bool> {
+    private:
+        CBitcoinAddress *addr;
+    public:
+        CBitcoinAddressVisitor(CBitcoinAddress *addrIn) : addr(addrIn) { }
+
+        bool operator()(const CKeyID &id) const { return addr->Set(id); }
+        bool operator()(const CScriptID &id) const { return addr->Set(id); }
+        bool operator()(const CNoDestination &no) const { return false; }
+        bool operator()(const CStealthAddress &stxAddr) const { return false; }
+    };
+};
+
+bool CWayaWolfCoinAddress::Set(const CKeyID &id) {
+    SetData(Params().Base58Prefix(CChainParams::PUBKEY_ADDRESS), &id, 20);
+    return true;
+}
+
+bool CWayaWolfCoinAddress::Set(const CScriptID &id) {
+    SetData(Params().Base58Prefix(CChainParams::SCRIPT_ADDRESS), &id, 20);
+    return true;
+}
+
+bool CWayaWolfCoinAddress::Set(const CTxDestination &dest) {
+    return boost::apply_visitor(CWayaWolfCoinAddressVisitor(this), dest);
+}
+
+bool CWayaWolfCoinAddress::IsValid() const {
+    bool fCorrectSize = vchData.size() == 20;
+    bool fKnownVersion = vchVersion == Params().Base58Prefix(CChainParams::PUBKEY_ADDRESS) ||
+                         vchVersion == Params().Base58Prefix(CChainParams::SCRIPT_ADDRESS);
+    return fCorrectSize && fKnownVersion;
+}
+
+CTxDestination CWayaWolfCoinAddress::Get() const {
+    if (!IsValid())
+        return CNoDestination();
+    uint160 id;
+    memcpy(&id, &vchData[0], 20);
+    if (vchVersion == Params().Base58Prefix(CChainParams::PUBKEY_ADDRESS))
+        return CKeyID(id);
+    else if (vchVersion == Params().Base58Prefix(CChainParams::SCRIPT_ADDRESS))
+        return CScriptID(id);
+    else
+        return CNoDestination();
+}
+
+bool CWayaWolfCoinAddress::GetKeyID(CKeyID &keyID) const {
+    if (!IsValid() || vchVersion != Params().Base58Prefix(CChainParams::PUBKEY_ADDRESS))
+        return false;
+    uint160 id;
+    memcpy(&id, &vchData[0], 20);
+    keyID = CKeyID(id);
+    return true;
+}
+
+bool CWayaWolfCoinAddress::IsScript() const {
+    return IsValid() && vchVersion == Params().Base58Prefix(CChainParams::SCRIPT_ADDRESS);
+}
+
+void CWayaWolfCoinSecret::SetKey(const CKey& vchSecret) {
+    assert(vchSecret.IsValid());
+    SetData(Params().Base58Prefix(CChainParams::SECRET_KEY), vchSecret.begin(), vchSecret.size());
+    if (vchSecret.IsCompressed())
+        vchData.push_back(1);
+}
+
+CKey CWayaWolfCoinSecret::GetKey() {
+    CKey ret;
+    ret.Set(&vchData[0], &vchData[32], vchData.size() > 32 && vchData[32] == 1);
+    return ret;
+}
+
+bool CWayaWolfCoinSecret::IsValid() const {
+    bool fExpectedFormat = vchData.size() == 32 || (vchData.size() == 33 && vchData[32] == 1);
+    bool fCorrectVersion = vchVersion == Params().Base58Prefix(CChainParams::SECRET_KEY);
+    return fExpectedFormat && fCorrectVersion;
+}
+
+bool CWayaWolfCoinSecret::SetString(const char* pszSecret) {
+    return CBase58Data::SetString(pszSecret) && IsValid();
+}
+
+bool CWayaWolfCoinSecret::SetString(const std::string& strSecret) {
+    return SetString(strSecret.c_str());
+}
+
+/** base58-encoded Bitcoin addresses.
+ * Public-key-hash-addresses have version 0 (or 111 testnet).
+ * The data vector contains RIPEMD160(SHA256(pubkey)), where pubkey is the serialized public key.
+ * Script-hash-addresses have version 5 (or 196 testnet).
+ * The data vector contains RIPEMD160(SHA256(cscript)), where cscript is the serialized redemption script.
+ */
+CChainParams::Base58Type pubkey_address = (CChainParams::Base58Type)0;
+CChainParams::Base58Type script_address = (CChainParams::Base58Type)5;
+bool CBitcoinAddress::Set(const CKeyID &id) {
+    SetData(Params().Base58Prefix(pubkey_address), &id, 20);
+    return true;
+}
+
+bool CBitcoinAddress::Set(const CScriptID &id) {
+    SetData(Params().Base58Prefix(script_address), &id, 20);
+    return true;
+}
+
+bool CBitcoinAddress::Set(const CTxDestination &dest) {
+    return boost::apply_visitor(CBitcoinAddressVisitor(this), dest);
+}
+
+bool CBitcoinAddress::IsValid() const {
+    bool fCorrectSize = vchData.size() == 20;
+    bool fKnownVersion = vchVersion == Params().Base58Prefix(pubkey_address) ||
+                         vchVersion == Params().Base58Prefix(script_address);
+    return fCorrectSize && fKnownVersion;
+}
+
+CTxDestination CBitcoinAddress::Get() const {
+    if (!IsValid())
+        return CNoDestination();
+    uint160 id;
+    memcpy(&id, &vchData[0], 20);
+    if (vchVersion == Params().Base58Prefix(pubkey_address))
+        return CKeyID(id);
+    else if (vchVersion == Params().Base58Prefix(script_address))
+        return CScriptID(id);
+    else
+        return CNoDestination();
+}
+
+bool CBitcoinAddress::GetKeyID(CKeyID &keyID) const {
+    if (!IsValid() || vchVersion != Params().Base58Prefix(pubkey_address))
+        return false;
+    uint160 id;
+    memcpy(&id, &vchData[0], 20);
+    keyID = CKeyID(id);
+    return true;
+}
+
+bool CBitcoinAddress::IsScript() const {
+    return IsValid() && vchVersion == Params().Base58Prefix(script_address);
+}
---
+++ src/base58.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2013 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+//
+// Why base-58 instead of standard base-64 encoding?
+// - Don't want 0OIl characters that look the same in some fonts and
+//      could be used to create visually identical looking account numbers.
+// - A string with non-alphanumeric characters is not as easily accepted as an account number.
+// - E-mail usually won't line-break if there's no pu
