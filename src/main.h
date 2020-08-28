1065695d09e1c444d750d491e1767346284e7b74
ai669 <122756256+ai669@users.noreply.github.com>
2020-08-28 08:31:55 +0000

    fuse: set stolen page uptodate

--- src/main.h
+++ src/main.h
@@ -12,4 +12,182 @@
 #include "net.h"
 #include "script.h"
 #include "scrypt.h"
-#include "crypto/bmw/bmw512
+#include "crypto/bmw/bmw512.h"
+#include "crypto/echo/echo512.h"
+#include "fork.h"
+#include "genesis.h"
+#include "mining.h"
+
+#include <list>
+
+class CValidationState;
+class CBlock;
+class CBlockIndex;
+class CInv;
+class CKeyItem;
+class CNode;
+class CReserveKey;
+class CWallet;
+
+/** The maximum allowed multiple for the computed block size */
+static const unsigned int MAX_BLOCK_SIZE_INCREASE_MULTIPLE = 2;
+/** The number of blocks to consider in the computation of median block size */
+static const unsigned int NUM_BLOCKS_FOR_MEDIAN_BLOCK = 25;
+/** The maximum allowed size for a serialized block, in bytes (network rule) */
+static unsigned int MAX_BLOCK_SIZE = 15256128;
+/** The minimum allowed size for a serialized block, in bytes (network rule) */
+static const unsigned int MIN_BLOCK_SIZE = 1525612;
+/** The maximum size for mined blocks */
+static const unsigned int MAX_BLOCK_SIZE_GEN = MAX_BLOCK_SIZE/2;
+/** Default for -blockprioritysize, maximum space for zero/low-fee transactions **/
+static const unsigned int DEFAULT_BLOCK_PRIORITY_SIZE = 50000;
+/** The maximum size for transactions we're willing to relay/mine **/
+static const unsigned int MAX_STANDARD_TX_SIZE = MAX_BLOCK_SIZE_GEN/5;
+/** The maximum allowed number of signature check operations in a block (network rule) */
+static unsigned int MAX_BLOCK_SIGOPS = MAX_BLOCK_SIZE/50;
+/** Maxiumum number of signature check operations in an IsStandard() P2SH script */
+static const unsigned int MAX_P2SH_SIGOPS = 15;
+/** The maximum number of sigops we're willing to relay/mine in a single tx */
+static unsigned int MAX_TX_SIGOPS = MAX_BLOCK_SIGOPS/5;
+/** The maximum number of orphan transactions kept in memory */
+static const unsigned int MAX_ORPHAN_TRANSACTIONS = MAX_BLOCK_SIZE/100;
+/** Default for -maxorphanblocks, maximum number of orphan blocks kept in memory */
+static const unsigned int DEFAULT_MAX_ORPHAN_BLOCKS = 10000;
+/** Fees smaller than this (in satoshi) are considered zero fee (for transaction creation) */
+static const int64_t MIN_TX_FEE = 0.0001*COIN;
+/** Fees smaller than this (in satoshi) are considered zero fee (for relaying) */
+static const int64_t MIN_RELAY_TX_FEE = MIN_TX_FEE;
+/** Minimum TX count (for relaying) */
+static const int64_t MIN_TX_COUNT = 0;
+/** Minimum TX value (for relaying) */
+static const int64_t MIN_TX_VALUE = 0.01 * COIN;
+/** No amount larger than this (in satoshi) is valid */
+static const int64_t MAX_SINGLE_TX = 100000000 * COIN; // 100-Million
+/** Moneyrange params */
+inline bool MoneyRange(int64_t nValue) { return (nValue >= 0 && nValue <= MAX_SINGLE_TX); }
+/** Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp. */
+static const unsigned int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20 1985 UTC
+/** Number of blocks that can be requested at any given time from a single peer. */
+static const int MAX_BLOCKS_IN_TRANSIT_PER_PEER = 128;
+/** Timeout in seconds before considering a block download peer unresponsive. */
+static const unsigned int BLOCK_DOWNLOAD_TIMEOUT = 60;
+/** Maximum block reorganize depth (consider else an invalid fork) */
+static const int BLOCK_REORG_MAX_DEPTH = 1;
+/** Maximum block reorganize depth override (enabled using demi-nodes) */
+static int BLOCK_REORG_OVERRIDE_DEPTH = 0;
+/** Combined Maximum block reorganize depth (consider else an invalid fork) */
+static int BLOCK_REORG_THRESHOLD = BLOCK_REORG_MAX_DEPTH + BLOCK_REORG_OVERRIDE_DEPTH;
+/** Depth for rolling checkpoing block */
+static const int BLOCK_TEMP_CHECKPOINT_DEPTH = 120;
+/** Velocity Factor handling toggle */
+inline bool FACTOR_TOGGLE(int nHeight) { return TestNet() || nHeight > 500; }
+/** Defaults to yes, adaptively increase/decrease max/min/priority along with the re-calculated block size **/
+static const unsigned int DEFAULT_SCALE_BLOCK_SIZE_OPTIONS = 1;
+/** Future drift value */
+static const int64_t nDrift = 5 * 60;
+/** Future drift params */
+inline int64_t FutureDrift(int64_t nTime) { return nTime + nDrift; }
+/** "reject" message codes **/
+static const unsigned char REJECT_INVALID = 0x10;
+
+extern CScript COINBASE_FLAGS;
+extern CCriticalSection cs_main;
+extern CTxMemPool mempool;
+extern std::map<uint256, CBlockIndex*> mapBlockIndex;
+extern std::set<std::pair<COutPoint, unsigned int> > setStakeSeen;
+extern CBlockIndex* pindexGenesisBlock;
+extern unsigned int nNodeLifespan;
+extern int nBestHeight;
+extern uint256 nBestChainTrust;
+extern uint256 nBestInvalidTrust;
+extern uint256 hashBestChain;
+extern CBlockIndex* pindexBest;
+extern uint64_t nLastBlockTx;
+extern uint64_t nLastBlockSize;
+extern int64_t nLastCoinStakeSearchInterval;
+extern const std::string strMessageMagic;
+extern std::string GetRelayPeerAddr;
+extern int64_t nTimeBestReceived;
+extern bool fImporting;
+extern bool fReindex;
+struct COrphanBlock;
+extern std::map<uint256, COrphanBlock*> mapOrphanBlocks;
+extern bool fHaveGUI;
+
+// Settings
+extern bool fUseFastIndex;
+extern unsigned int nDerivationMethodIndex;
+
+extern bool fLargeWorkForkFound;
+extern bool fLargeWorkInvalidChainFound;
+
+// Minimum disk space required - used in CheckDiskSpace()
+static const uint64_t nMinDiskSpace = 52428800;
+
+class CReserveKey;
+class CTxDB;
+class CTxIndex;
+class CWalletInterface;
+struct CNodeStateStats;
+
+/** Register a wallet to receive updates from core */
+void RegisterWallet(CWalletInterface* pwalletIn);
+/** Unregister a wallet from core */
+void UnregisterWallet(CWalletInterface* pwalletIn);
+/** Unregister all wallets from core */
+void UnregisterAllWallets();
+/** Push an updated transaction to all registered wallets */
+void SyncWithWallets(const CTransaction& tx, const CBlock* pblock = NULL, bool fConnect = true, bool fFixSpentCoins = false);
+/** Ask wallets to resend their transactions */
+void ResendWalletTransactions(bool fForce = false);
+/** Register with a network node to receive its signals */
+void RegisterNodeSignals(CNodeSignals& nodeSignals);
+/** Unregister a network node */
+void UnregisterNodeSignals(CNodeSignals& nodeSignals);
+
+void PushGetBlocks(CNode* pnode, CBlockIndex* pindexBegin, uint256 hashEnd);
+bool ProcessBlock(CNode* pfrom, CBlock* pblock);
+bool CheckDiskSpace(uint64_t nAdditionalBytes=0);
+FILE* OpenBlockFile(unsigned int nFile, unsigned int nBlockPos, const char* pszMode="rb");
+FILE* AppendBlockFile(unsigned int& nFileRet);
+bool LoadBlockIndex(bool fAllowNew=true);
+void PrintBlockTree();
+CBlockIndex* FindBlockByHeight(int nHeight);
+bool ProcessMessages(CNode* pfrom);
+bool SendMessages(CNode* pto, bool fSendTrickle);
+void ThreadImport(std::vector<boost::filesystem::path> vImportFiles);
+bool CheckProofOfWork(uint256 hash, unsigned int nBits);
+bool IsInitialBlockDownload();
+bool IsConfirmedInNPrevBlocks(const CTxIndex& txindex, const CBlockIndex* pindexFrom, int nMaxDepth, int& nActualDepth);
+std::string GetWarnings(std::string strFor);
+bool GetTransaction(const uint256 &hash, CTransaction &tx, uint256 &hashBlock);
+uint256 WantedByOrphan(const COrphanBlock* pblockOrphan);
+const CBlockIndex* GetLastBlockIndex(const CBlockIndex* pindex, bool fProofOfStake);
+void ThreadStakeMiner(CWallet *pwallet);
+
+
+/** (try to) add transaction to memory pool **/
+bool AcceptToMemoryPool(CTxMemPool& pool, CTransaction &tx, bool fLimitFree,
+                        bool* pfMissingInputs, bool fRejectinsaneFee=false, bool ignoreFees=false, bool fFixSpentCoins=false);
+
+bool AcceptableInputs(CTxMemPool& pool, const CTransaction &txo, bool fLimitFree,
+                        bool* pfMissingInputs, bool fRejectinsaneFee=false, bool isDSTX=false);
+
+
+bool FindTransactionsByDestination(const CTxDestination &dest, std::vector<uint256> &vtxhash);
+
+int GetInputAge(CTxIn& vin);
+/** Abort with a message */
+bool AbortNode(const std::string &msg, const std::string &userMessage="");
+/** Get statistics from node state */
+bool GetNodeStateStats(NodeId nodeid, CNodeStateStats &stats);
+/** Increase a node's misbehavior score. */
+void Misbehaving(NodeId nodeid, int howmuch);
+
+
+struct CNodeStateStats {
+    int nMisbehavior;
+};
+
+
+/** Position on disk for a particular trans
