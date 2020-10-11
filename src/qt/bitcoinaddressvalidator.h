91520043b4087259b41ac45023c36b9dc1f8cd66
ai669 <122756256+ai669@users.noreply.github.com>
2020-10-11 08:14:04 +0000

    Final tweaks

---
+++ src/qt/bitcoinaddressvalidator.h
@@ -0,0 +1,9 @@
+#ifndef BITCOINADDRESSVALIDATOR_H
+#define BITCOINADDRESSVALIDATOR_H
+
+#include <QValidator>
+
+/** Base48 entry widget validator.
+   Corrects near-miss characters and refuses characters that are no part of base48.
+ */
+class WayaWolfCoi
