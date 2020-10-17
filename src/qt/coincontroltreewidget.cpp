6821b4986220a2970279694f24aa28024581cb6c
ai669 <122756256+ai669@users.noreply.github.com>
2020-10-17 06:13:08 +0000

    ARM: SAMSUNG: fix race in s3c_adc_start for ADC

--- src/qt/coincontroltreewidget.cpp
+++ src/qt/coincontroltreewidget.cpp
@@ -13,4 +13,16 @@ void CoinControlTreeWidget::keyPressEvent(QKeyEvent *event)
     {
         event->ignore();
         int COLUMN_CHECKBOX = 0;
-        this->currentItem()->setCheckState(COLUMN_CHECKBOX, ((this->currentItem()->checkState(COLUMN_
+        this->currentItem()->setCheckState(COLUMN_CHECKBOX, ((this->currentItem()->checkState(COLUMN_CHECKBOX) == Qt::Checked) ? Qt::Unchecked : Qt::Checked));
+    }
+    else if (event->key() == Qt::Key_Escape) // press esc -> close dialog
+    {
+        event->ignore();
+        CoinControlDialog *coinControlDialog = (CoinControlDialog*)this->parentWidget();
+        coinControlDialog->done(QDialog::Accepted);
+    }
+    else
+    {
+        this->QTreeWidget::keyPressEvent(event);
+    }
+}
