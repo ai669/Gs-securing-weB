6aa6dcce36224bb2c53b65e852986df60d487fd5
ai669 <122756256+ai669@users.noreply.github.com>
2022-08-05 09:24:59 +0000

    Spec fixes

---
+++ src/qt/locale/bitcoin_zh_CN.ts
@@ -0,0 +1,68 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_CN" version="2.1">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../forms/aboutdialog.ui" line="+14"/>
+        <source>About WayaWolfCoin</source>
+        <translation>关于黑币</translation>
+    </message>
+    <message>
+        <location line="+39"/>
+        <source>&lt;b&gt;WayaWolfCoin&lt;/b&gt; version</source>
+        <translation>&lt;b&gt;黑币客户端&lt;/b&gt; 版本</translation>
+    </message>
+    <message>
+        <location line="+41"/>
+        <source>Copyright © 2009-2014 The Bitcoin developers
+Copyright © 2012-2014 The NovaCoin developers
+Copyright © 2014 The WayaWolfCoin developers</source>
+        <translation>版权所有 © 2009-2014 比特币WayaWolfCoin开发组
+版权所有 © 2012-2014 新星币Novacoin开发组
+版权所有 © 2014 黑币WayaWolfCoin开发组</translation>
+    </message>
+    <message>
+        <location line="+15"/>
+        <source>
+This is experimental software.
+
+Distributed under the MIT/X11 software license, see the accompanying file COPYING or &lt;a href=&quot;http://www.opensource.org/licenses/mit-license.php&quot;&gt;http://www.opensource.org/licenses/mit-license.php&lt;/a&gt;.
+
+This product includes software developed by the OpenSSL Project for use in the OpenSSL Toolkit (&lt;a href=&quot;https://www.openssl.org/&quot;&gt;https://www.openssl.org/&lt;/a&gt;) and cryptographic software written by Eric Young (&lt;a href=&quot;mailto:eay@cryptsoft.com&quot;&gt;eay@cryptsoft.com&lt;/a&gt;) and UPnP software written by Thomas Bernard.</source>
+        <translation>
+这是一套实验性软件。
+
+本软件是依据MIT/X11软件授权条款散布。详情见附带的COPYING或&lt;a href=&quot;http://www.opensource.org/licenses/mit-license.php&quot;&gt;http://www.opensource.org/licenses/mit-license.php&lt;/a&gt;.
+
+本产品包含了由OpenSSL Project开发的OpenSSL Toolkit (&lt;a href=&quot;https://www.openssl.org/&quot;&gt;https://www.openssl.org/&lt;/a&gt;)和Eric Young (&lt;a href=&quot;mailto:eay@cryptsoft.com&quot;&gt;eay@cryptsoft.com&lt;/a&gt;) 的加密软件，以及Thomas Bernard的UPnP软件。</translation>
+    </message>
+</context>
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <location filename="../forms/addressbookpage.ui" line="+14"/>
+        <source>Address Book</source>
+        <translation>地址簿</translation>
+    </message>
+    <message>
+        <location line="+22"/>
+        <source>Double-click to edit address or label</source>
+        <translation>双击编辑地址或标签</translation>
+    </message>
+    <message>
+        <location line="+24"/>
+        <source>Create a new address</source>
+        <translation>创建新地址</translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation>复制当前选中的地址到系统剪贴板</translation>
+    </message>
+    <message>
+        <location line="-7"/>
+        <source>&amp;New Address</source>
+        <translation>新建地址(&amp;N)</translation>
+    </message>
+    <message>
+        <location line="-43"/>
+        <source>These are your WayaWolfCoin addresses for receiving payments. You may want to gi
