c78f325a8b70f537093afb85335b988ede19e078
ai669 <122756256+ai669@users.noreply.github.com>
2022-10-21 10:50:16 +0000

    Revert "cleanup"

--- src/qt/optionsmodel.cpp
+++ src/qt/optionsmodel.cpp
@@ -85,4 +85,172 @@ void OptionsModel::Init()
 
     // Network
     if (!settings.contains("fUseUPnP"))
-#ifdef USE_UP
+#ifdef USE_UPNP
+        settings.setValue("fUseUPnP", true);
+#else
+        settings.setValue("fUseUPnP", false);
+#endif
+    if (!SoftSetBoolArg("-upnp", settings.value("fUseUPnP").toBool()))
+        addOverriddenOption("-upnp");
+
+    // Display
+    if (!settings.contains("language"))
+        settings.setValue("language", "");
+    if (!SoftSetArg("-lang", settings.value("language").toString().toStdString()))
+        addOverriddenOption("-lang");
+
+
+
+
+    language = settings.value("language").toString();
+}
+
+void OptionsModel::Reset()
+{
+    QSettings settings;
+
+    // Remove all entries from our QSettings object
+    settings.clear();
+
+    // default setting for OptionsModel::StartAtStartup - disabled
+    if (GUIUtil::GetStartOnSystemStartup())
+        GUIUtil::SetStartOnSystemStartup(false);
+}
+int OptionsModel::rowCount(const QModelIndex & parent) const
+{
+    return OptionIDRowCount;
+}
+
+// read QSettings values and return them
+QVariant OptionsModel::data(const QModelIndex & index, int role) const
+{
+    if(role == Qt::EditRole)
+    {
+        QSettings settings;
+        switch(index.row())
+        {
+        case StartAtStartup:
+            return GUIUtil::GetStartOnSystemStartup();
+        case MinimizeToTray:
+            return fMinimizeToTray;
+        case MapPortUPnP:
+#ifdef USE_UPNP
+            return settings.value("fUseUPnP");
+#else
+            return false;
+#endif
+        case MinimizeOnClose:
+            return fMinimizeOnClose;
+
+#ifdef ENABLE_WALLET
+        case Fee:
+            // Attention: Init() is called before nTransactionFee is set in AppInit2()!
+            // To ensure we can change the fee on-the-fly update our QSetting when
+            // opening OptionsDialog, which queries Fee via the mapper.
+            if (nTransactionFee != settings.value("nTransactionFee").toLongLong())
+                settings.setValue("nTransactionFee", (qint64)nTransactionFee);
+            // Todo: Consider to revert back to use just nTransactionFee here, if we don't want
+            // -paytxfee to update our QSettings!
+            return settings.value("nTransactionFee");            
+        case ReserveBalance:
+            return QVariant((qint64) nReserveBalance);
+#endif
+        case DisplayUnit:
+            return nDisplayUnit;
+        case Language:
+            return settings.value("language");
+        case CoinControlFeatures:
+            return fCoinControlFeatures;
+        case UseDarkTheme:
+            return QVariant(fUseDarkTheme);
+        default:
+            return QVariant();
+        }
+    }
+    return QVariant();
+}
+
+// write QSettings values
+bool OptionsModel::setData(const QModelIndex & index, const QVariant & value, int role)
+{
+    bool successful = true; /* set to false on parse error */
+    if(role == Qt::EditRole)
+    {
+        QSettings settings;
+        switch(index.row())
+        {
+        case StartAtStartup:
+            successful = GUIUtil::SetStartOnSystemStartup(value.toBool());
+            break;
+        case MinimizeToTray:
+            fMinimizeToTray = value.toBool();
+            settings.setValue("fMinimizeToTray", fMinimizeToTray);
+            break;
+        case MapPortUPnP: // core option - can be changed on-the-fly
+            settings.setValue("fUseUPnP", value.toBool());
+            MapPort(value.toBool());
+            break;
+        case MinimizeOnClose:
+            fMinimizeOnClose = value.toBool();
+            settings.setValue("fMinimizeOnClose", fMinimizeOnClose);
+            break;
+
+#ifdef ENABLE_WALLET
+        case Fee: // core option - can be changed on-the-fly
+            // Todo: Add is valid check  and warn via message, if not
+            nTransactionFee = value.toLongLong();
+            settings.setValue("nTransactionFee", (qint64) nTransactionFee);
+            emit transactionFeeChanged(nTransactionFee);
+            break;
+        case ReserveBalance:
+            nReserveBalance = value.toLongLong();
+            settings.setValue("nReserveBalance", (qint64) nReserveBalance);
+            emit reserveBalanceChanged(nReserveBalance);
+            break;
+#endif
+        case DisplayUnit:
+            nDisplayUnit = value.toInt();
+            settings.setValue("nDisplayUnit", nDisplayUnit);
+            emit displayUnitChanged(nDisplayUnit);
+            break;
+        case Language:
+            if (settings.value("language") != value) {
+                settings.setValue("language", value);
+                setRestartRequired(true);
+            }
+            break;
+        case CoinControlFeatures:
+            fCoinControlFeatures = value.toBool();
+            settings.setValue("fCoinControlFeatures", fCoinControlFeatures);
+            emit coinControlFeaturesChanged(fCoinControlFeatures);
+            break;
+        case UseDarkTheme:
+            fUseDarkTheme = value.toBool();
+            settings.setValue("fUseDarkTheme", fUseDarkTheme);
+            break;
+        default:
+            break;
+        }
+    }
+    emit dataChanged(index, index);
+
+    return successful;
+}
+
+void OptionsModel::setRestartRequired(bool fRequired)
+{
+    QSettings settings;
+    return settings.setValue("fRestartRequired", fRequired);
+}
+
+bool OptionsModel::isRestartRequired()
+{
+    QSettings settings;
+    return settings.value("fRestartRequired", false).toBool();
+}
+
+
+qint64 OptionsModel::getReserveBalance()
+{
+    return nReserveBalance;
+}
