ae34b1607557d06102d1db3caaa51786e83c847e
ai669 <122756256+ai669@users.noreply.github.com>
2022-10-30 19:57:24 +0000

    39: update Jenkinsfile

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -466,4 +466,122 @@ void OverviewPage::updatePoSstat(bool stat)
         }
         else if(nExpectedTime > 59)
         {
-           nExpectedTime =
+           nExpectedTime = nExpectedTime / 60;
+           Qseconds = " Minute(s)";
+        }
+        ui->lbTime->show();
+
+        int nHeight = pindexBest->nHeight;
+
+        // Net weight percentage
+        double nStakePercentage = (double)nWeight / (double)nNetworkWeight * 100;
+        double nNetPercentage = (100 - (double)nStakePercentage);
+        if(nWeight > nNetworkWeight)
+        {
+            nStakePercentage = (double)nNetworkWeight / (double)nWeight * 100;
+            nNetPercentage = (100 - (double)nStakePercentage);
+        }
+        // Sync percentage
+        // (1498887616 - 1498884018 / 100) / (1498887616 - 1467348018 / 100) * 100
+        double nTimeLapse = (GetTime() - pindexBest->GetBlockTime()) / 100;
+        double nTimetotalLapse = (GetTime() - Params().GenesisBlock().GetBlockTime()) / 100;
+        double nSyncPercentage = 100 - (nTimeLapse / nTimetotalLapse * 100);
+
+        QString QStakePercentage = QString::number(nStakePercentage, 'f', 2);
+        QString QNetPercentage = QString::number(nNetPercentage, 'f', 2);
+        QString QSyncPercentage = QString::number(nSyncPercentage, 'f', 2);
+        QString QTime = clientModel->getLastBlockDate().toString();
+        QString QBlockHeight = QString::number(nHeight);
+        QString QExpect = QString::number(nExpectedTime, 'f', 0);
+        QString QStaking = "Disabled";
+        QString QStakeEN = "Not Staking";
+        QString QSynced = "Ready (Synced)";
+        QString QSyncing = "Not Ready (Syncing)";
+        QString QFailed = "Not Connected";
+        ui->estnxt->setText(QExpect + Qseconds);
+        ui->stkstat->setText(QStakeEN);
+        ui->lbHeight->setText(QBlockHeight);
+        ui->clStat->setText("<font color=\"red\">" + QFailed + "</font>");
+        if(nExpectedTime == 0)
+        {
+            QExpect = "Not Staking";
+            ui->estnxt->setText(QExpect);
+        }
+        if(staking)
+        {
+            QStakeEN = "Staking";
+            ui->stkstat->setText(QStakeEN);
+        }
+        if(GetBoolArg("-staking", true))
+        {
+            QStaking = "Enabled";
+        }
+        if((GetTime() - pindexBest->GetBlockTime()) < 45 * 60)
+        {
+            QSyncPercentage = "100";
+            nSyncPercentage = 100;
+        }
+        ui->lbTime->setText(QTime);
+        ui->stken->setText(QStaking);
+        ui->urweight_2->setText(QStakePercentage + "%");
+        ui->netweight_2->setText(QNetPercentage + "%");
+        ui->sncStatus->setText(QSyncPercentage + "%");
+        if(nSyncPercentage == 100)
+            ui->clStat->setText("<font color=\"green\">" + QSynced + "</font>");
+        if(nSyncPercentage != 100)
+        {
+            if(clientModel->getNumConnections() > 0)
+                ui->clStat->setText("<font color=\"orange\">" + QSyncing + "</font>");
+        }
+        return;
+    }
+}
+
+void OverviewPage::setCntBlocks(int pseudo)
+{
+    pseudo = pindexBest->GetBlockTime();
+
+    if(pseudo > Params().GenesisBlock().GetBlockTime())
+    {
+        updatePoSstat(true);
+    }
+
+    return;
+}
+
+void OverviewPage::setCntConnections(int count)
+{
+    ui->lbcon->setText(QString::number(count));
+
+    int64_t pseudo = pindexBest->GetBlockTime();
+
+    if(pseudo > Params().GenesisBlock().GetBlockTime())
+    {
+        updatePoSstat(true);
+    }
+
+}
+
+void OverviewPage::on_vwalltx_clicked()
+{
+    WayaWolfCoinGUI::gotoHistoryPage_static();
+}
+
+void OverviewPage::setLockIconLocked(){
+    ui->lockstatusLabel->setPixmap(QPixmap(":/icons/lock_closed_toolbar"));
+}
+
+void OverviewPage::setLockIconUnlocked(){
+    ui->lockstatusLabel->setPixmap(QPixmap(":/icons/lock_open_toolbar"));
+}
+
+void OverviewPage::setLockStatus(){
+    // lockstatusLabel
+    if(settingsStatus)
+    {
+        setLockIconLocked();
+    } else
+    {
+        setLockIconUnlocked();
+    }
+}
