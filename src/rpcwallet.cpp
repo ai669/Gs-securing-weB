e9bd1e2ea976b5d4f0f86f611dd1d6cd4a074455
ai669 <122756256+ai669@users.noreply.github.com>
2023-02-15 19:11:10 +0000

    test cases

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -2354,4 +2354,270 @@ Value importstealthaddress(const Array& params, bool fHelp)
             throw runtime_error("Could not decode scan secret as hex or base58.");
     };
 
- 
+    if (IsHex(sSpendSecret))
+    {
+        vchSpendSecret = ParseHex(sSpendSecret);
+    } else
+    {
+        if (!DecodeBase58(sSpendSecret, vchSpendSecret))
+            throw runtime_error("Could not decode spend secret as hex or base58.");
+    };
+
+    if (vchScanSecret.size() != 32)
+        throw runtime_error("Scan secret is not 32 bytes.");
+    if (vchSpendSecret.size() != 32)
+        throw runtime_error("Spend secret is not 32 bytes.");
+
+
+    ec_secret scan_secret;
+    ec_secret spend_secret;
+
+    memcpy(&scan_secret.e[0], &vchScanSecret[0], 32);
+    memcpy(&spend_secret.e[0], &vchSpendSecret[0], 32);
+
+    ec_point scan_pubkey, spend_pubkey;
+    if (SecretToPublicKey(scan_secret, scan_pubkey) != 0)
+        throw runtime_error("Could not get scan public key.");
+
+    if (SecretToPublicKey(spend_secret, spend_pubkey) != 0)
+        throw runtime_error("Could not get spend public key.");
+
+
+    CStealthAddress sxAddr;
+    sxAddr.label = sLabel;
+    sxAddr.scan_pubkey = scan_pubkey;
+    sxAddr.spend_pubkey = spend_pubkey;
+
+    sxAddr.scan_secret = vchScanSecret;
+    sxAddr.spend_secret = vchSpendSecret;
+
+    Object result;
+    bool fFound = false;
+    // -- find if address already exists
+    //std::set<CStealthAddress>::iterator it;
+    //for (it = pwalletMain->stealthAddresses.begin(); it != pwalletMain->stealthAddresses.end(); ++it)
+    BOOST_FOREACH(CStealthAddress it, pwalletMain->stealthAddresses)
+    {
+        CStealthAddress &sxAddrIt = const_cast<CStealthAddress&>(it); //*it);
+        if (sxAddrIt.scan_pubkey == sxAddr.scan_pubkey
+            && sxAddrIt.spend_pubkey == sxAddr.spend_pubkey)
+        {
+            if (sxAddrIt.scan_secret.size() < 1)
+            {
+                sxAddrIt.scan_secret = sxAddr.scan_secret;
+                sxAddrIt.spend_secret = sxAddr.spend_secret;
+                fFound = true; // update stealth address with secrets
+                break;
+            };
+
+            result.push_back(Pair("result", "Import failed - stealth address exists."));
+            return result;
+        };
+    };
+
+    if (fFound)
+    {
+        result.push_back(Pair("result", "Success, updated " + sxAddr.Encoded()));
+    } else
+    {
+        pwalletMain->stealthAddresses.insert(sxAddr);
+        result.push_back(Pair("result", "Success, imported " + sxAddr.Encoded()));
+    };
+
+
+    if (!pwalletMain->AddStealthAddress(sxAddr))
+        throw runtime_error("Could not save to wallet.");
+
+    return result;
+}
+
+
+Value sendtostealthaddress(const Array& params, bool fHelp)
+{
+    if (fHelp || params.size() < 2 || params.size() > 5)
+        throw runtime_error(
+            "sendtostealthaddress <stealth_address> <amount> [comment] [comment-to] [narration]\n"
+            "<amount> is a real and is rounded to the nearest 0.000001"
+            + HelpRequiringPassphrase());
+
+    if (pwalletMain->IsLocked())
+        throw JSONRPCError(RPC_WALLET_UNLOCK_NEEDED, "Error: Please enter the wallet passphrase with walletpassphrase first.");
+
+    std::string sEncoded = params[0].get_str();
+    CAmount nAmount = AmountFromValue(params[1]);
+
+    std::string sNarr;
+    if (params.size() > 4 && params[4].type() != null_type && !params[4].get_str().empty())
+        sNarr = params[4].get_str();
+
+    if (sNarr.length() > 24)
+        throw runtime_error("Narration must be 24 characters or less.");
+
+    CStealthAddress sxAddr;
+
+    Object result;
+
+    if (!sxAddr.SetEncoded(sEncoded))
+    {
+        result.push_back(Pair("result", "Invalid WayaWolfCoin stealth address."));
+        return result;
+    };
+
+
+
+    CWalletTx wtx;
+    if (params.size() > 2 && params[2].type() != null_type && !params[2].get_str().empty())
+        wtx.mapValue["comment"] = params[2].get_str();
+    if (params.size() > 3 && params[3].type() != null_type && !params[3].get_str().empty())
+        wtx.mapValue["to"]      = params[3].get_str();
+
+
+
+    std::string sError;
+    if (!pwalletMain->SendStealthMoneyToDestination(sxAddr, nAmount, sNarr, wtx, sError))
+        throw JSONRPCError(RPC_WALLET_ERROR, sError);
+
+    return wtx.GetHash().GetHex();
+
+    result.push_back(Pair("result", "Not implemented yet."));
+
+    return result;
+}
+
+Value scanforalltxns(const Array& params, bool fHelp)
+{
+    if (fHelp || params.size() > 1)
+        throw runtime_error(
+            "scanforalltxns [fromHeight]\n"
+            "Scan blockchain for owned transactions.");
+
+    Object result;
+    int32_t nFromHeight = 0;
+
+    CBlockIndex *pindex = pindexGenesisBlock;
+
+
+    if (params.size() > 0)
+        nFromHeight = params[0].get_int();
+
+
+    if (nFromHeight > 0)
+    {
+        pindex = mapBlockIndex[hashBestChain];
+        while (pindex->nHeight > nFromHeight
+            && pindex->pprev)
+            pindex = pindex->pprev;
+    };
+
+    if (pindex == NULL)
+        throw runtime_error("Genesis Block is not set.");
+
+    {
+        LOCK2(cs_main, pwalletMain->cs_wallet);
+
+        pwalletMain->MarkDirty();
+
+        pwalletMain->ScanForWalletTransactions(pindex, true);
+        pwalletMain->ReacceptWalletTransactions();
+    }
+
+    result.push_back(Pair("result", "Scan complete."));
+
+    return result;
+}
+
+Value scanforstealthtxns(const Array& params, bool fHelp)
+{
+    if (fHelp || params.size() > 1)
+        throw runtime_error(
+            "scanforstealthtxns [fromHeight]\n"
+            "Scan blockchain for owned stealth transactions.");
+
+    Object result;
+    uint32_t nBlocks = 0;
+    uint32_t nTransactions = 0;
+    int32_t nFromHeight = 0;
+
+    CBlockIndex *pindex = pindexGenesisBlock;
+
+
+    if (params.size() > 0)
+        nFromHeight = params[0].get_int();
+
+
+    if (nFromHeight > 0)
+    {
+        pindex = mapBlockIndex[hashBestChain];
+        while (pindex->nHeight > nFromHeight
+            && pindex->pprev)
+            pindex = pindex->pprev;
+    };
+
+    if (pindex == NULL)
+        throw runtime_error("Genesis Block is not set.");
+
+    // -- locks in AddToWalletIfInvolvingMe
+
+    bool fUpdate = true; // todo: option?
+
+    pwalletMain->nStealth = 0;
+    pwalletMain->nFoundStealth = 0;
+
+    while (pindex)
+    {
+        nBlocks++;
+        CBlock block;
+        block.ReadFromDisk(pindex, true);
+
+        BOOST_FOREACH(CTransaction& tx, block.vtx)
+        {
+
+            nTransactions++;
+
+            pwalletMain->AddToWalletIfInvolvingMe(tx, &block, fUpdate);
+        };
+
+        pindex = pindex->pnext;
+    };
+
+    printf("Scanned %u blocks, %u transactions\n", nBlocks, nTransactions);
+    printf("Found %u stealth transactions in blockchain.\n", pwalletMain->nStealth);
+    printf("Found %u new owned stealth transactions.\n", pwalletMain->nFoundStealth);
+
+    char cbuf[256];
+    snprintf(cbuf, sizeof(cbuf), "%u new stealth transactions.", pwalletMain->nFoundStealth);
+
+    result.push_back(Pair("result", "Scan complete."));
+    result.push_back(Pair("found", std::string(cbuf)));
+
+    return result;
+}
+
+Value cclistcoins(const Array& params, bool fHelp)
+{
+    if (fHelp || params.size() != 0)
+        throw runtime_error(
+            "cclistcoins\n"
+                        "CoinControl: list your spendable coins and their information\n");
+
+        Array result;
+
+        std::vector<COutput> vCoins;
+    pwalletMain->AvailableCoins(vCoins);
+
+        BOOST_FOREACH(const COutput& out, vCoins)
+    {
+                Object coutput;
+                int64_t nHeight = nBestHeight - out.nDepth;
+                CBlockIndex* pindex = FindBlockByHeight(nHeight);
+
+                CTxDestination outputAddress;
+                ExtractDestination(out.tx->vout[out.i].scriptPubKey, outputAddress);
+                coutput.push_back(Pair("Address", CWayaWolfCoinAddress(outputAddress).ToString()));
+                coutput.push_back(Pair("Output Hash", out.tx->GetHash().ToString()));
+                coutput.push_back(Pair("blockIndex", out.i));
+                double dAmount = double(out.tx->vout[out.i].nValue) / double(COIN);
+                coutput.push_back(Pair("Value", dAmount));
+                coutput.push_back(Pair("Confirmations", int(out.nDepth)));
+                double dAge = double(GetTime() - pindex->nTime);
+                coutput.push_back(Pair("Age (days)", (dAge/(60*60
